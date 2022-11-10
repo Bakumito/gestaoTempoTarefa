58fdf46a3984ccd67508e8448f89b6421fe5731d
Bakumito <thomazfalbo@outlook.com>
2022-11-10 11:43:38 -0300

    funcionalidade adicionar linha criada

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,12 @@ const cronometro = document.getElementById('contador')
 const iniciar = document.getElementById('iniciar')
 const pausar = document.getElementById('pausar')
 const finalizar = document.getElementById('finalizar')
+const btnTeste = document.getElementById('botaoTeste')
+const btnTeste2 = document.getElementById('botaoTeste2')
+const inputNome = document.getElementById('inputNome')
+const inputAtividade = document.getElementById('inputAtividade')
+const inputTipoAtividade = document.getElementById('inputTipoAtividade')
+const tbody = document.getElementById('tabelaCorpo')
 let segundos = 0
 let contador
 
@@ -40,3 +46,71 @@ finalizar.addEventListener('click', e => {
   clearInterval(contador)
   segundos = 0
 })
+
+let linhas = []
+
+const inputer = () => {
+  return {
+    nome: inputNome.value,
+    atividade: inputAtividade.value,
+    tipoAtividade: inputTipoAtividade.value,
+    tempo: cronometro.innerText
+  }
+}
+
+class Linha {
+  constructor(nome, atividade, tipoAtividade, tempo) {
+    this.nome = nome
+    this.atividade = atividade
+    this.tipoAtividade = tipoAtividade
+    this.tempo = tempo
+    this.editar = 'editar'
+    this.excluir = 'excluir'
+  }
+}
+
+const fnLinhas = (nome, atividade, tipoAtividade, tempo) => {
+  linhas.push(new Linha(nome, atividade, tipoAtividade, tempo))
+  console.log(linhas)
+}
+
+function fnAddLinha() {
+  const novaLinha = tbody.insertRow(-1)
+  const novoNome = novaLinha.insertCell(0)
+  const novaAtividade = novaLinha.insertCell(1)
+  const novoTipoAtividade = novaLinha.insertCell(2)
+  const novoTempo = novaLinha.insertCell(3)
+  const novoEditar = novaLinha.insertCell(4)
+  const novoExcluir = novaLinha.insertCell(5)
+  const textoNome = document.createTextNode(linhas[linhas.length - 1].nome)
+  const textoAtividade = document.createTextNode(
+    linhas[linhas.length - 1].atividade
+  )
+  const textoTipoAtividade = document.createTextNode(
+    linhas[linhas.length - 1].tipoAtividade
+  )
+  const textoTempo = document.createTextNode(linhas[linhas.length - 1].tempo)
+  const textoEditar = document.createTextNode(linhas[linhas.length - 1].editar)
+  const textoExcluir = document.createTextNode(
+    linhas[linhas.length - 1].excluir
+  )
+  novoNome.appendChild(textoNome)
+  novaAtividade.appendChild(textoAtividade)
+  novoTipoAtividade.appendChild(textoTipoAtividade)
+  novoTempo.appendChild(textoTempo)
+  novoEditar.appendChild(textoEditar)
+  novoExcluir.appendChild(textoExcluir)
+}
+
+btnTeste.addEventListener('click', e => {
+  fnLinhas(
+    inputer().nome,
+    inputer().atividade,
+    inputer().tipoAtividade,
+    inputer().tempo
+  )
+
+  fnAddLinha()
+})
+
+btnTeste2.addEventListener('click', e => {})
